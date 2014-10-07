e28c4ecff1abeb8d2a6c4e06e9e79e5b9d1a37d0
Mathijs Baaijens <mathijs.baaijens@gmail.com>
2014-10-07 15:37:12 +0200

    Verder de opdracht afgemaakt

--- file.R
+++ file.R
@@ -2,26 +2,36 @@
 tree.grow <- function (x, y, nmin, minleaf) 
 {
   if (length(y) == 1 || impurity(y) == 0) {
-    cat("Impurity == 0")        
+    y[1]  
   } else {
     
     
     split <- dataSplit (x, y)
     
-    cat("Best data split: ", split, "\n")
+    #cat("Best data split: ", split, "\n")
+    
+    splitcol <- split[1]
+    splitval <- split[3]
+    
+    splitdat <- x[[splitcol]]  
+    smaller <- splitdat <= splitval
+    larger <- splitdat > splitval
     
     #    cat(split)
-    left <- x[x[,split[1]] <= split[3],]
-    right <- x[x[,split[1]] > split[3],]
+    left <- x[smaller,]
+    right <- x[larger,]
     
-    yleft <- y[x[,split[1]] <= split[3]]
-    yright <- y[x[,split[1]] > split[3]]
+    yleft <- y[smaller]
+    yright <- y[larger]
     
-    cat("left: ", data.frame(left), "\n")
-    cat("right: ", data.frame(right), "\n")
+    #rownames(left) <- NULL
+    #rownames(right) <- NULL    
     
-    cat("yleft: ", yleft, "\n")
-    cat("yright: ", yright, "\n")
+    #cat("left: ", data.frame(left), "\n")
+    #cat("right: ", data.frame(right), "\n")
+    
+    #cat("yleft: ", yleft, "\n")
+    #cat("yright: ", yright, "\n")
 
     
     cat ("Stepping into recursion")
@@ -30,9 +40,12 @@ tree.grow <- function (x, y, nmin, minleaf)
     
     
     
-    tree.grow(data.frame(left), yleft, nmin, minleaf)
-    tree.grow(data.frame(right), yright, nmin, minleaf)
+    l <- tree.grow(left, yleft, nmin, minleaf)
+    r <- tree.grow(right, yright, nmin, minleaf)
     
+    list(split=c(splitcol, splitval), left=l, right=r)
+    
+    #((1,1), (1,1))
     #list(left = left, right = right, yleft = yleft, yright = yright)
     
   }
@@ -44,22 +57,72 @@ tree.grow <- function (x, y, nmin, minleaf)
   #Filter(x, x[,split[1]] < split[3])
 }
 
+tree.classify <- function (tree, cases) {
+  as.vector(apply(cases, 1, function(x) { classifyCase(tree, x)}))
+}
+
+classifyCase <- function (tree, x) {
+  if (typeof(tree) == "integer") {
+    tree
+  } else {
+    col <- tree$split[1]
+    val <- tree$split[2]
+    if (x[col] <= val) {
+      classifyCase(tree$left, x)
+    } else {
+      classifyCase(tree$right, x)
+    }
+  }  
+}
+
+#tree.classify <- function (tree, x) {
+#  
+#  if (typeof(tree) == "integer") {
+#    rep(tree, nrow(x))
+#  } else {
+#  
+#    col <- tree$split[1]
+#    val <- tree$split[2]
+#  
+#    tol <- x[col] <= val
+#    tor <- x[col] > val
+#    
+#    l <- x[tol,]
+#    r <- x[tor,]
+#  
+#    c(tree.classify (tree$left, l), tree.classify (tree$right, r))
+#  }
+  
+  
+  #resl <- 
+  #print(l)
+  #print(r)
+#}
 
 
 
 dataSplit <- function (data, y) 
 {
+ 
+  #print(typeof(data))
   # calculate best split for every column
   # in order to see which attribute split
   # leads to highest impurity reduction.
   reducs <- apply(data, 2, function(x) {
-    split <- bestSplit(x, y)  ### x incorrect here
-    cat("bestSplit: ", split, "\n")
-    red <- impurityReduction(x, y, split)
-    c(red, split)
+    if (length(unique(x)) == 1) {
+      c(0,0)
+    } else {
+      split <- bestSplit(x, y)  ### x incorrect here
+      #cat("bestSplit: ", split, "\n")
+      red <- impurityReduction(x, y, split)
+      c(red, split)
+      #c(0,0)
+    }
   })
   
-  #print(reducs)
+  #print(typeof(data))
+  #print(typeof(y))
+  #print(typeof(reducs))
   
   
   # find best attribute split
@@ -73,35 +136,35 @@ dataSplit <- function (data, y)
 
 bestSplit <- function (x, y) 
 {
-  cat('xa: ', x, "\n")
-  cat('ya: ', y, "\n")
+  #cat('xa: ', x, "\n")
+  #cat('ya: ', y, "\n")
   
   # sort input
   y <- sortBy(y, x)
   x <- sort(x) 
   
-  cat('x1: ', x, "\n")
-  cat('y1: ', y, "\n")
+  #cat('x1: ', x, "\n")
+  #cat('y1: ', y, "\n")
   
   # splits to consider
   splits <- splits(x)
-  cat("splits: ", splits, "\n")
+  #cat("splits: ", splits, "\n")
   
-  cat('x2: ', x, "\n")
-  cat('y2: ', y, "\n")
+  #cat('x2: ', x, "\n")
+  #cat('y2: ', y, "\n")
   
   # calculate impurity reduction for each split
   rds <- sapply(splits, function (split) {
     ## XXX  conflict: x and y have changed here
-    cat('x3: ', x, "\n")
-    cat('y3: ', y, "\n")
+    #cat('x3: ', x, "\n")
+    #cat('y3: ', y, "\n")
     impurityReduction(x, y, split)
   })
   
-  cat("rds: ", rds, "\n")
+  #cat("rds: ", rds, "\n")
   
   # return the best split value
-  splits[rds == max(rds)]
+  splits[rds == max(rds)][1]
 }
 
 impurity <- function (v) 
@@ -126,7 +189,7 @@ impurityReduction <- function (x, y, split)
 
 splits <- function(v) {
   v <- sort(unique(v))
-  cat("array: ", v, "\n")
+  #cat("array: ", v, "\n")
   xs <- v[-length(v)] # init
   ys <- v[-1]         # tail
   zs <- ys + xs       # zipWith (+) (tail y) (init y)
@@ -141,8 +204,8 @@ frac <- function (v)
 
 sortBy <- function (x, y) 
 {
-  cat('x0: ', x, "\n")
-  cat('y0: ', y, "\n")
+  #cat('x0: ', x, "\n")
+  #cat('y0: ', y, "\n")
   x[sort.int(y, index.return = TRUE)$ix]
 }
 
