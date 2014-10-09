ecbca29691c9a401a62520e1e36ee8543c8c2e9b
Jarno Le Conté <jarno.leconte@me.com>
2014-10-09 17:41:04 +0200

    ionosphere dataset

--- test.R
+++ test.R
@@ -20,4 +20,14 @@ pima.y <- pima.data[,9]  # vector of class labels only
 
 pima.tree <- tree.grow(pima.x, pima.y, 20, 5)   # build tree
 pima.class <- tree.classify(pima.x, pima.tree)  # predict / classification
-pima.confusion <- table(pima.class, pima.y)     # confusion table (XXX I think this differs to much!!!!)
+pima.confusion <- table(pima.class, pima.y)     # confusion table (correct)
+
+# bank marketing
+
+iono.data <- read.csv("~/dev/school/mdm/introduction/ionosphere.data", header = FALSE)
+iono.x <- iono.data[-35]  # without class labels
+iono.y <- iono.data[,35]  # vector of class labels only
+
+iono.tree <- tree.grow(iono.x, iono.y, 1, 1)   # build tree (XXX we have to tweak parameters)
+iono.class <- tree.classify(iono.x, iono.tree)  # predict / classification
+iono.confusion <- table(iono.class, iono.y)     # confusion table
