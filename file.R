a3cf4e4b85cb5e2f529f7e30409658f918c5b75b
Mathijs Baaijens <mathijs.baaijens@gmail.com>
2014-10-10 15:15:41 +0200

    Cleanup

--- file.R
+++ file.R
@@ -6,8 +6,7 @@
 # Mathijs Baaijens (3542068)
 #
 
-tree.grow <- function (x, y, nmin, minleaf) 
-{
+tree.grow <- function (x, y, nmin, minleaf) {
   # Grows a classification tree based on the input training data.
   #
   # Args:
@@ -15,37 +14,40 @@ tree.grow <- function (x, y, nmin, minleaf)
   #   y: Training data classs labels.
   #   nmin : Minimum number of observations that a node must contain at least,
   #          for it to be allowed to be split.
-  #   minleaf : s the minimum number of observations required for a leaf node.
+  #   minleaf : The minimum number of observations required for a leaf node.
   #
   # Returns:
   #   Classification tree based on input training data.
   
-  if (length(y) < nmin || impurity(y) == 0) {
+  if (length(y) < nmin) {
     
-    # Leaf node.
-    # Determine most probable class label from all observations in this leaf.
-    determine.classLabel(y)
+    determine.optimal.label(y)
     
   } else {
-      
-    # Find the column and attribute value for the optimal split.
-    split <- optimal.split (x, y, minleaf)
+    
+    split <- find.optimal.split (x, y, minleaf)
     
     if (is.null(split)) {
-      determine.classLabel(y)
+      
+      # If no split was found (e.g. due to impurity = 0 or the minleaf constraint)
+      # this becomes a leaf node.
+      determine.optimal.label(y)
+      
     } else {
+      
       # Select the rows classified in the left and right sides of the tree.
-      smaller <- x[[split$attr]] <= split$val
-      larger <- x[[split$attr]] > split$val
+      smaller <- x[split$attr] <= split$val
+      larger <- x[split$attr] > split$val
     
       # Build the left and right branches of the classification tree.
       left <- tree.grow(x[smaller,], y[smaller], nmin, minleaf)
       right <- tree.grow(x[larger,], y[larger], nmin, minleaf)
       
-      # Return the classification tree.
       list(attr = split$attr, val = split$val, left = left, right = right)     
+      
     }
   }  
+  
 }
 
 tree.classify <- function (x, tr) {
@@ -65,6 +67,17 @@ tree.classify <- function (x, tr) {
 }
 
 tree.classify.case <- function(tr, x) { 
+  
+  # Predicts class labels for the input row based on the input classification
+  # tree.
+  #
+  # Args:
+  #   x: Vector containing the attribute values for the cases to classify.  
+  #   tr: Classification tree to use to predict class labels.
+  #
+  # Returns:
+  #   Predicted class labels for the input data.
+  
   if (typeof(tr) == "integer") {
     tr
   } else {
@@ -76,79 +89,73 @@ tree.classify.case <- function(tr, x) {
   }  
 }
 
-optimal.split <- function (data, y, minleaf) 
-{
- 
-  # calculate best split for every column
-  # in order to see which attribute split
-  # leads to highest impurity reduction.
-  reducs <- apply(data, 2, function(x) {
-    #if (length(unique(x)) == 1) {
-    #  c(0,0)
-    #} else {
-      #NULL
-      split <- bestSplit(x, y, minleaf)
-      if (!is.null(split)) {
-        split
-      } else {
-        c(0,0)
-      }
-      #cat(split)
-      #  red <- impurityReduction(x, y, split)
-      #  c(red, split)
-      #} else {
-      #  c(0,0)
-      #}
-    #}
-  })
+find.optimal.split <- function (data, y, minleaf) {
+  # Finds the column and value for splitting the input dataset that results
+  # in the largest impurity reduction.
+  #
+  # Args:
+  #   x: Dataset.
+  #   y: Class labels.
+  #   minleaf : The minimum number of observations required for a leaf node.
+  #
+  # Returns:
+  #   List containing the column number, impurity reduction and value for the
+  #   optimal split.
+  
+  # Determine the optimal split for every column in the dataset.
+  reducs <- lapply(1:ncol(data), function(col) {
     
-  # find best attribute split
-  red <- max(reducs[1,])
-  if (red > 0.0) {
-    attr <- which(reducs[1,] == red)
-    split <- reducs[2,attr]
-  
-    # return best split
-    list(attr = attr[1], red = red[1], val = split[1])
-  }
-}
-
-bestSplit <- function (x, y, minleaf) 
-{  
-  # sort input
-  y <- sortBy(y, x)
-  x <- sort(x) 
+    splits <- find.split.values(data[,col], minleaf)
     
-  # splits to consider
-  splits <- splits(x, minleaf)
-
-  if (length(splits) == 0) {
-    #return()
-    #NA
-  } else {
-  
-  # calculate impurity reduction for each split
-  rds <- sapply(splits, function (split) {
-    impurityReduction(x, y, split)
+    if (length(splits) == 0) {
+      
+      # No splits for this column (due to no impurity or minleaf constraint).
+      list(attr = col, red = NA, val = NA)
+      
+    } else {
+      
+      # Calculate impurity reduction for all possible split values.
+      rds <- sapply(splits, function (split) {
+        determine.impurity.reduction(data[,col], y, split)
+      })
+      
+      # Return the split with the largest impurity reduction.
+      list(attr = col, red = max(rds), val = splits[which.max(rds)])
+            
+    }
   })
-  
-    # return the best split value
-    maxrds <- max(rds)
-    c(maxrds, splits[rds == maxrds][1])
-  }
-}
 
-impurity <- function (v) 
-{   
-  # impurity
-  # i(t): p(0|t)(1 - p(0|t))
+  # Select the optimal column to split on (if any) and return it.
+  bestsplit <- which.max(lapply(reducs, function(x) x$red))
+  if (length(bestsplit) != 0) {
+    reducs[[bestsplit]]
+  }
+
+}
+impurity <- function (v) {   
+  # Determines the impurity (Gini coefficient) for a vector of class labels
+  #
+  # Args:
+  #   v: Vector of class labels
+  #
+  # Returns:
+  #   Impurity (Gini coefficient) for the input vector.
   
   p0 = frac(v==0)
   p0 * (1 - p0) 
 }
 
-impurityReduction <- function (x, y, split) 
-{
+determine.impurity.reduction <- function (x, y, split) {
+  # Determines the impurity reduction for a given split.
+  #
+  # Args:
+  #   v: Vector of attributes to split
+  #   y: Vector of class labels for the input data.
+  #   split: Split value.
+  #
+  # Returns:
+  #   Impurity (Gini coefficient) for the given split.
+  
   pi_l <- frac(x <= split)
   pi_r <- frac(x > split)
   
@@ -158,32 +165,57 @@ impurityReduction <- function (x, y, split)
   impurity(y) - pi_l*impur_l - pi_r*impur_r
 }
 
-splits <- function(v, minleaf) {
+find.split.values <- function(v, minleaf) {
+  # Determines the possible split values for the input vector.
+  #
+  # Args:
+  #   v: Vector of attributes to split
+  #   minleaf : The minimum number of observations required for a leaf node.
+  #
+  # Returns:
+  #   Vector of possible split values.
+  
   if (minleaf <= length(v) / 2) {
+    
+    # Drop the first and last 'minleaf' elements so that only splits that
+    # respect 'minleaf' will be returned.
     v <- v[(minleaf):(length(v) - minleaf + 1)]
+    
+    # Only consider splits between unique values.
     v <- sort(unique(v))
+    
+    # Calculate the mid-value for every possible split.
     zs <- (v[-1] + v[-length(v)])
     zs / 2
+    
   } else {
+    
+    # No splits possible.
     return(NULL)
   }
 }
 
-frac <- function (v) 
-{
-  # count the fraction TRUE elements
+frac <- function (v){
+  # Returns the fraction of true elements in an input vector
+  #
+  # Args:
+  #   v: Input vector.
+  #
+  # Returns:
+  #   Fraction of true elements in the input.
   length(v[v]) / length(v)
 }
 
-sortBy <- function (x, y) 
-{
-  x[sort.int(y, index.return = TRUE)$ix]
-}
-
-
-# Return classification label with highest probability
-determine.classLabel = function(classLabels) {
-  freqTable <- table(classLabels)
-  as.integer(names(which.max(freqTable)))
+determine.optimal.label = function(labels) {
+  # Determines the most common class label in a vector of class labels
+  #
+  # Args:
+  #   labels: Vector of class label.
+  #
+  # Returns:
+  #   Most common label.
+  
+  freqtable <- table(labels)
+  as.integer(names(which.max(freqtable)))
 }
 
